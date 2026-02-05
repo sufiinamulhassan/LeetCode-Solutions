350abbbd1ac06ddee32f5e81b00e7bf5eef58d1a
Inam05 <inamulhassan.tech@gmail.com>
2026-02-05 15:43:30 +0500

    31.next-permutation.py has been solved...

---
+++ Python/31.next-permutation.py
@@ -0,0 +1,22 @@
+# Time:  O(n)
+# Space: O(1)
+class Solution(object):
+    def nextPermutation(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: None Do not return anything, modify nums in-place instead.
+        """
+        k, l = -1, 0
+        for i in xrange(len(nums)-1):
+            if nums[i] < nums[i+1]:
+                k = i
+
+        if k == -1:
+            nums.reverse()
+            return
+
+        for i in xrange(k+1, len(nums)):
+            if nums[i] > nums[k]:
+                l = i
+        nums[k], nums[l] = nums[l], nums[k]
+        nums[k+1:] = nums[:k:-1]
