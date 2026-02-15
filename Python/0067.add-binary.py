539567730f62ccd8465387cbe51a52492133e392
Inam05 <inamulhassan.tech@gmail.com>
2026-02-15 07:16:18 +0500

    0067.add-binary.py has been added.

---
+++ Python/0067.add-binary.py
@@ -0,0 +1,21 @@
+# Time:  O(n)
+# Space: O(1)
+
+class Solution(object):
+    # @param a, a string
+    # @param b, a string
+    # @return a string
+    def addBinary(self, a, b):
+        result, carry, val = "", 0, 0
+        for i in xrange(max(len(a), len(b))):
+            val = carry
+            if i < len(a):
+                val += int(a[-(i + 1)])
+            if i < len(b):
+                val += int(b[-(i + 1)])
+            carry, val = divmod(val, 2)
+            result += str(val)
+        if carry:
+            result += str(carry)
+        return result[::-1]
+
