2ffe7f1d5c66e6a31bc8c66c3f5da84fb7a94731
Inam05 <inamulhassan.tech@gmail.com>
2026-02-18 07:43:49 +0500

    0014.longest-common-prefix.py has been added.

---
+++ Python/0014.longest-common-prefix.py
@@ -0,0 +1,21 @@
+# Time:  O(n*m)
+# Space: O(1)
+
+class Solution(object):
+    def longestCommonPrefix(self, strs):
+        """
+        :type strs: List[str]
+        :rtype: str
+        """
+        
+        if len(strs) == 0:
+            return ""
+
+        base = strs[0]
+        
+        for i in range(len(base)):
+            for word in strs[1:]:
+                if i == len(word) or word[i] != base[i]:
+                    return base[:i]
+        
+        return base
